e538c1f4d4f7d204760b6bd74511d909c93917c5
Tech-Kat <katpelle@pursuit.org>
2023-03-12 22:21:29 -0400

    finished

--- solution.js
+++ solution.js
@@ -1,72 +1,72 @@
 const { nums, words } = require("./data/data.js");
 
 // Every
-const isEveryNumGreaterThan2 = () => {
-  //
+const isEveryNumGreaterThan2 = (arr) => {
+  return arr.every((a) => a >= 2);
 };
 
-const isEveryWordShorterThan7 = () => {
-  //
+const isEveryWordShorterThan7 = (arr) => {
+  return arr.every((a) => a.length < 7)
 };
 
 // Filter
 
-const arrayLessThan5 = () => {
-  //
+const arrayLessThan5 = (arr) => {
+  return arr.filter((a) => a < 5)
 };
 
-const arrayOddLengthWords = () => {
-  //
+const arrayOddLengthWords = (arr) => {
+  return arr.filter((a) => a.length % 2 == 1)
 };
 
 // Find
 
-const firstValDivisibleBy4 = () => {
-  //
+const firstValDivisibleBy4 = (arr) => {
+  return arr.find((a) => a % 4 == 0)
 };
 
-const firstWordLongerThan4Char = () => {
-  //
+const firstWordLongerThan4Char = (arr) => {
+  return arr.find((a) => a.length > 4 )
 };
 
 // Find Index
 
-const firstNumIndexDivisibleBy3 = () => {
-  //
+const firstNumIndexDivisibleBy3 = (arr) => {
+ return arr.findIndex((a) => a % 3 == 0)
 };
 
-const firstWordIndexLessThan2Char = () => {
-  //
+const firstWordIndexLessThan2Char = (arr) => {
+  return arr.findIndex((a) => a.length < 2)
 };
 
 // For Each
 
-const logValuesTimes3 = () => {
-  //
+const logValuesTimes3 = (arr) => {
+  return arr.forEach((a) => a * 3)
 };
 
-const logWordsWithExclamation = () => {
-  //
+const logWordsWithExclamation = (arr) => {
+  return arr.forEach((a) => a + "!")
 };
 
 // Map
 
-const arrayValuesSquaredTimesIndex = () => {
-  //
+const arrayValuesSquaredTimesIndex = (arr) => {
+  return arr.map((a, index) => (a**2) * index )
 };
 
-const arrayWordsUpcased = () => {
-  //
+const arrayWordsUpcased = (arr) => {
+  return arr.map((a) => a.toUpperCase())
 };
 
 // Some
 
-const areSomeNumsDivisibleBy7 = () => {
-  //
+const areSomeNumsDivisibleBy7 = (arr) => {
+  return arr.some((a) => a % 7 == 0)
 };
 
-const doSomeWordsHaveAnA = () => {
-  //
+const doSomeWordsHaveAnA = (arr) => {
+  return arr.some((b) => b.includes("a"))
 };
 
 module.exports = {
